3cc9b15a5f177b23e434df9e5cdf9af70ee33cdf
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2024-05-24 00:20:58 -0300

    feat: snackbar message added

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -7,8 +7,10 @@ import {
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -24,6 +26,8 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
   productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
   form = new FormGroup({
     title: new FormControl<string>('', {
@@ -38,7 +42,13 @@ export class CreateComponent {
         title: this.form.controls.title.value,
       })
       .subscribe(() => {
-        alert('Sucesso!!');
+        this.matSnackBar.open('Produto criado com sucesso!', 'Ok', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+
+        this.router.navigateByUrl('/');
       });
   }
 }
